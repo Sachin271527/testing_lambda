10b199c614134bb97fdfa116fde366ba34284370
sachin271527 <sachinsustainabilty@gmail.com>
2025-06-19 14:05:54 +0530

    chnages in lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
 def lambda_handler(event, context):
-    return {"message": "Hello from GitHub Deployed Lambda function!"}
+    return {"message": "GitHub Deployed Lambda function!"}
 # lambda_handler(1,2)
 
