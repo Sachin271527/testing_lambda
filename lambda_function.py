192ed329d5ebeaf0fe23c45134933c5602ff0499
sachin271527 <sachinsustainabilty@gmail.com>
2025-06-19 11:45:53 +0530

    Trigger redeploy test

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
 def lambda_handler(event, context):
-    return {"message": "Hello from GitHub Deployed Lambda!"}
+    return {"message": "Hello from GitHub Deployed Lambda function!"}
 # lambda_handler(1,2)
 
